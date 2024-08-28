44315a2d2dafe1455c4c2b2d45427f57fde97b05
Natasha Masyago <natalia.masyago@gmail.com>
2024-08-28 17:47:08 +0000

    Calculator: asking user if they want another calculation

--- PY101_Lesson_2/calculator.py
+++ PY101_Lesson_2/calculator.py
@@ -1,8 +1,4 @@
-# X Ask the user for the first number.
-# X Ask the user for the second number.
-# X Ask the user for an operation to perform.
-# X Perform the operation on the two numbers.
-# X Print the results to the terminal.
+# Ask user if they want to perfrorm another calculation
 
 def prompt(message):
     print(f"==> {message}")
@@ -18,38 +14,57 @@ def invalid_number(number_str):
 
 prompt('Welcome to Calculator!')
 
-prompt("What's the first number?")
-number1 = input()
-
-
-while invalid_number(number1):
-    prompt("Hmm... that doesn't look like a valid number.")
+def calc_main():
+    prompt("What's the first number?")
     number1 = input()
 
-prompt("What's the second number?")
-number2 = input()
 
-while invalid_number(number2):
-    prompt("Hmm... that doesn't look like a valid number.")
+    while invalid_number(number1):
+        prompt("Hmm... that doesn't look like a valid number.")
+        number1 = input()
+    
+    prompt("What's the second number?")
     number2 = input()
-
-prompt("""What operation would you like to perform?
-    1) Add 2) Subtract 3) Multiply 4) Divide""")
-operation = input()
-
-while operation not in ["1", "2", "3", "4"]:
-    prompt('You must choose 1, 2, 3, or 4')
+    
+    while invalid_number(number2):
+        prompt("Hmm... that doesn't look like a valid number.")
+        number2 = input()
+    
+    prompt("""What operation would you like to perform?
+        1) Add 2) Subtract 3) Multiply 4) Divide""")
     operation = input()
+    
+    while operation not in ["1", "2", "3", "4"]:
+        prompt('You must choose 1, 2, 3, or 4')
+        operation = input()
+    
+    
+    match operation:
+        case '1':   # '1' represents addition
+            output = int(number1) + int(number2)
+        case '2': # '2' represents subtraction
+            output = int(number1) - int(number2)
+        case '3': # '3' represents multiplication
+            output = int(number1) * int(number2)
+        case '4': # '4' represents division
+            output = int(number1) / int(number2)
+    
+    print(f'The result is {output}.')
 
+    def calc_continue():
+        print("Do you want to perform another calculation? y/n ")
+        response = input()
+        match response:
+            case "y":
+                calc_main()
+            case "n":
+                print("Session ended. Goodbye!")
+                exit()
+            case _:
+                print("You must respond y or n.")
+                calc_continue()
+                
+    calc_continue()
 
-match operation:
-    case '1':   # '1' represents addition
-        output = int(number1) + int(number2)
-    case '2': # '2' represents subtraction
-        output = int(number1) - int(number2)
-    case '3': # '3' represents multiplication
-        output = int(number1) * int(number2)
-    case '4': # '4' represents division
-        output = int(number1) / int(number2)
-
-print(f'The result is {output}.')
+    
+calc_main()
